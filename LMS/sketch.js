2d0b03ea0deb15035accc449c24eb0d9cd9ac5b4
HBJenn <163226314+HBJenn@users.noreply.github.com>
2025-04-22 18:32:50 -0600

    Add files via upload

---
+++ LMS/sketch.js
@@ -0,0 +1,33 @@
+let classifier;
+let video;
+let label = "Esperando...";
+
+
+function preload() {
+  classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/nCN3m3jr2/", {flipped:true});
+}
+
+function gotResults(results) {
+  console.log(results);
+  label = results[0].label;
+}
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO, {flipped:true});
+  video.hide();
+  classifier.classifyStart(video, gotResults);
+}
+function draw(){
+  background(220);
+  image(video, 0, 0, width, height);
+
+  rectMode(CENTER);
+  fill(0);
+  rect(width /2, height - 50, width, 50);
+  textSize(32);
+  fill(255);
+  textAlign(CENTER,CENTER);
+  noStroke();
+  text(label, width /2, height -50);
+
+}
